ddf6510e048214c392169db5f5910bcbbe3af669
bob sagat <wiiqwertyuiop@gmail.com>
2024-03-16 12:46:57 -0400

    grid

--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-	"fmt"
-	utils "gosnakego/utils"
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-func main() {
-	list := utils.LinkedList[int]{}
-	list.
-		Prepend(2).
-		Prepend(60).
-		Prepend(81).
-		Prepend(80).
-		Prepend(43).
-		RemoveFirstValueMatch(80).
-		Prepend(62)
+const cellLength = 10
+const borderSize = 1
+const numberOfCells = 10
 
-	if val, found := list.PopWithValueOnIndex(1); found {
-		fmt.Println(val)
-		fmt.Println(
-			list.
-				Prepend(33).
-				Prepend(42).
-				ReadValueOnIndex(3))
+const screenSize = (borderSize+cellLength)*numberOfCells + borderSize
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Draw Grid
+	for i := 1; i <= screenSize; i += (cellLength + borderSize) {
+		vector.StrokeLine(screen, float32(i), 0, float32(i), float32(screenSize), float32(borderSize), color.White, false)
+		vector.StrokeLine(screen, 0, float32(i), float32(screenSize), float32(i), float32(borderSize), color.White, false)
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return screenSize, screenSize // Square screen
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Go Snake Go")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
 	}
 }
